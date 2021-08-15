c4fac9326a19f6b5dac5506d824d99c98c4f2278
brunoczim <brunoczim@gmail.com>
2021-08-15 11:24:53 -0300

    changing CI, using local storage to save input

--- src/onload.js
+++ src/onload.js
@@ -1,10 +1,25 @@
 window.onload = function () {
   var aitlInput = document.getElementById("textarea-aitl");
   var ipaInput = document.getElementById("textarea-ipa");
+
+  function save() {
+    localStorage.setItem("aitl-input", aitlInput.value);
+    localStorage.setItem("aitl-ipa-input", ipaInput.value);
+  }
+
+  function load() {
+    aitlInput.value = localStorage.getItem("aitl-input") || "";
+    ipaInput.value = localStorage.getItem("aitl-ipa-input") || "";
+  }
+
+  load();
+
   aitlInput.onkeyup = function () {
     ipaInput.value = toIpa(aitlInput.value);
+    save();
   };
   ipaInput.onkeyup = function () {
     aitlInput.value = fromIpa(ipaInput.value);
+    save();
   };
 };
